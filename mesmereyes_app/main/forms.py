1aec3af6694966f19e72c3565b3d9d9d001142cb
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-10 13:31:54 -0500

    Added form to add a new activity to the db

--- mesmereyes_app/main/forms.py
+++ mesmereyes_app/main/forms.py
@@ -1,1 +1,18 @@
-# Create your forms here.
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, DateField, SelectField, SubmitField, TextAreaField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from wtforms.validators import DataRequired, Length, ValidationError
+from mesmereyes_app.models import InterActivity, Level
+
+class InterActivityForm(FlaskForm):
+    """Form to add a new interActivity."""
+    title = StringField('Sketch Title',
+        validators=[DataRequired(), Length(min=1, max=80)])
+    # TODO check if url is valid
+    url = StringField('Sketch URL',
+        validators=[DataRequired()])
+    cc_attribution = TextAreaField('CC Attribution',
+        validators=[DataRequired()])
+    visual_complexity = SelectField('Visual Complexity', choices=Level.choices())
+    visual_contrast = SelectField('Visual Contrast', choices=Level.choices())
+    submit = SubmitField('Submit')
--- mesmereyes_app/main/routes.py
+++ mesmereyes_app/main/routes.py
@@ -1,6 +1,7 @@
 
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from sqlalchemy import func
+from mesmereyes_app.main.forms import InterActivityForm
 from mesmereyes_app.models import InterActivity
 from mesmereyes_app.extensions import app, db
 
@@ -22,3 +23,21 @@ def interactivities():
     all_interactivities = InterActivity.query.all()
     return render_template('interactivities.html', all_interactivities=all_interactivities)
 
+@main.route('/new_interactivity', methods=['GET', 'POST'])
+def new_interactivity():
+    form = InterActivityForm()
+
+    # if form was submitted and contained no errors
+    if form.validate_on_submit(): 
+        new_interactivity = InterActivity(
+            title=request.form['title'],
+            url=request.form['url'],
+            cc_attribution=request.form['cc_attribution'],
+            visual_complexity=request.form['visual_complexity'],
+            visual_contrast=request.form['visual_contrast']
+            )
+        db.session.add(new_interactivity)
+        db.session.commit()
+        flash('Interactivity added successfully.')
+        return redirect(url_for('main.interactivities'))
+    return render_template('new_interactivity.html', form=form)
