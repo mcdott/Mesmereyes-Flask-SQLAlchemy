6cc0e95fd9db3d690363b1918dc1e3f4f4967f41
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-10 11:16:05 -0500

    Added the basic view and route for the interactivities page

--- mesmereyes_app/main/routes.py
+++ mesmereyes_app/main/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, request, render_template, redirect, url_for, flash
-from mesmereyes_app.models import InterActivity
 
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from sqlalchemy import func
+from mesmereyes_app.models import InterActivity
 from mesmereyes_app.extensions import app, db
+
 
 main = Blueprint('main', __name__)
 
@@ -11,13 +13,12 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def homepage():
+    sample_interactivities = InterActivity.query.order_by(func.random()).limit(3).all()
+    return render_template('home.html', sample_interactivities=sample_interactivities)
+
+
+@main.route('/interactivities', methods=['GET', 'POST'])
+def interactivities():
     all_interactivities = InterActivity.query.all()
-    print(all_interactivities)
-    return render_template('home.html', all_interactivities=all_interactivities)
-
-
-# @main.route('/')
-# def homepage():
-#     return render_template('home.html')
-
+    return render_template('interactivities.html', all_interactivities=all_interactivities)
 
