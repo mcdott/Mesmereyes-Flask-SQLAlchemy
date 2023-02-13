7f96284ba81b089b5f0f987b88197b222ceefa97
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-13 18:17:49 -0500

    Added route tests for the homepage for cases where the user is logged in and not logged in

--- mesmereyes_app/main/tests.py
+++ mesmereyes_app/main/tests.py
@@ -38,3 +38,69 @@ def create_user():
     user = User(username='me1', password=password_hash)
     db.session.add(user)
     db.session.commit()
+
+#################################################
+# Tests
+#################################################
+
+class MainTests(unittest.TestCase):
+ 
+    def setUp(self):
+        """Executed prior to each test."""
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+ 
+    def test_homepage_logged_out(self):
+        """Test that the doodles show up on the homepage."""
+        # Set up
+        create_doodles()
+        create_user()
+
+        # Make a GET request
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that page contains all of the things we expect
+        response_text = response.get_data(as_text=True)
+        self.assertIn('Doodle 1', response_text)
+        self.assertIn('Doodle 2', response_text)
+        self.assertIn('Log In', response_text)
+        self.assertIn('Sign Up', response_text)
+        
+        # Check that the page doesn't contain things we don't expect
+        # (these should be shown only to logged in users)
+        self.assertNotIn('add doodle', response_text)
+        self.assertNotIn('my playlists', response_text)
+        self.assertNotIn('add playlist', response_text)
+        self.assertNotIn('me1', response_text)
+        self.assertNotIn('Log Out', response_text)
+
+    def test_homepage_logged_in(self):
+        """Test that the books show up on the homepage."""
+        # Set up
+        create_doodles()
+        create_user()
+        login(self.app, 'me1', 'password')
+
+        # Make a GET request
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that page contains all of the things we expect
+        response_text = response.get_data(as_text=True)
+        self.assertIn('add doodle', response_text)
+        self.assertIn('my playlists', response_text)
+        self.assertIn('add playlist', response_text)
+        self.assertIn('me1', response_text)
+        self.assertIn('Log Out', response_text)
+   
+
+        # Check that the page doesn't contain things we don't expect
+        # (these should be shown only to logged out users)
+        self.assertNotIn('Log In', response_text)
+        self.assertNotIn('Sign Up', response_text)
