d8225c4971313048e93fa74226f4ff12ae7566b5
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-15 17:16:37 -0500

    Added tests for the 'doodles' and 'new_doodle' routes

--- mesmereyes_app/main/tests.py
+++ mesmereyes_app/main/tests.py
@@ -104,3 +104,98 @@ class MainTests(unittest.TestCase):
         # (these should be shown only to logged out users)
         self.assertNotIn('Log In', response_text)
         self.assertNotIn('Sign Up', response_text)
+
+
+    def test_doodles_logged_out(self):
+        """Test that the doodles show up on the doodles page."""
+        # Set up
+        create_doodles()
+        create_user()
+
+        # Make a GET request
+        response = self.app.get('/doodles', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that page contains all of the things we expect
+        response_text = response.get_data(as_text=True)
+        self.assertIn('Doodle 1', response_text)
+        self.assertIn('Doodle 2', response_text)
+        self.assertIn('Log In', response_text)
+        self.assertIn('Sign Up', response_text)
+        
+        # Check that the page doesn't contain things we don't expect
+        # (these should be shown only to logged in users)
+        self.assertNotIn('add doodle', response_text)
+        self.assertNotIn('my playlists', response_text)
+        self.assertNotIn('add playlist', response_text)
+        self.assertNotIn('me1', response_text)
+        self.assertNotIn('Log Out', response_text)
+
+    def test_new_doodle(self):
+        """Test that a doodle can be added."""
+        # Use helper functions to create a user and log in
+        create_user()
+        login(self.app, 'me1', 'password')
+
+        # Make a POST request with data
+        response = self.app.post('/doodles/new', data=dict(
+            title='Doodle 3',
+            url='https://www.google.com',
+            cc_attribution='google.com'
+        ), follow_redirects=True)
+
+        # Check that doodle was added to database
+        doodle = Doodle.query.filter_by(title='Doodle 3').first()
+        self.assertIsNotNone(doodle)
+        self.assertEqual(doodle.title, 'Doodle 3')
+
+# @main.route('/doodle/<int:doodle_id>')
+# def doodle(doodle_id):
+#     doodle = Doodle.query.get(doodle_id)
+#     return render_template('doodle_details.html', doodle=doodle)
+
+# @main.route('/playlists', methods=['GET', 'POST'])
+# @login_required
+# def playlists():
+#     user_playlists = Playlist.query.filter_by(user=current_user).all()
+#     return render_template('playlists.html', user_playlists=user_playlists)
+
+# @main.route('/new_playlist', methods=['GET', 'POST'])
+# @login_required
+# def new_playlist():
+#     form = PlaylistForm()
+
+#     # if form was submitted and contained no errors
+#     if form.validate_on_submit(): 
+#         new_playlist = Playlist(
+#             name=request.form['name'],
+#             user=current_user
+#             )
+#         db.session.add(new_playlist)
+#         db.session.commit()
+#         flash('Playlist added successfully!')
+#         return redirect(url_for('main.playlists'))
+#     return render_template('new_playlist.html', form=form)
+
+
+# @main.route('/add_doodle_to_playlist/<int:doodle_id>', methods=['GET', 'POST'])
+# def add_doodle_to_playlist(doodle_id):
+#     doodle = Doodle.query.get(doodle_id)
+#     form = AddDoodleToPlaylistForm()
+#     form.playlist_id.choices = [(p.id, p.name) for p in Playlist.query.filter_by(user=current_user).all()]
+   
+
+#     if form.validate_on_submit():
+#         playlist = Playlist.query.get(form.playlist_id.data)
+#         playlist.doodles.append(doodle)
+#         db.session.commit()
+#         flash(f'Doodle "{doodle.title}" added to playlist "{playlist.name}"!')
+#         return redirect(url_for('main.playlists'))
+
+#     return render_template('add_doodle_to_playlist.html', doodle=doodle, form=form)
+
+
+# @main.route('/playlist/<int:playlist_id>')
+# def playlist(playlist_id):
+#     playlist = Playlist.query.get(playlist_id)
+#     return render_template('playlist_details.html', playlist=playlist)
